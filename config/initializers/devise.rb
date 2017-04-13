40fc0903c5eda0f8f54678651ded2e6cc846e9cd
Mark Stein <mystycs@gmail.com>
2017-04-13 03:09:05 +0000

    add omniauth for facebook

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -252,6 +252,7 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 
+  config.omniauth :facebook, "409064429449781", "63499a15369fb63a16b28657527a215d"
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'my_music_playlist#index', as: 'my_music_playlist'
+
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
 
 end
